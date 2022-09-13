765281b3c5a9fc5d083e0344ef3b71acf574d1f3
Artem Martynov <amerta90@mail.ru>
2022-09-13 08:50:09 +0300

    test final

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello peoples!");
-        System.out.println("Godby my Friend");
         System.out.println("test");
+        System.out.println("");
+        System.out.println("");
     }
+
 }
