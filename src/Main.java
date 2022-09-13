6609ef8d148bcd29d51a4c85bea54e936b9eafb3
Artem Martynov <amerta90@mail.ru>
2022-09-13 08:51:08 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello peoples!");
         System.out.println("Godby my Friend");
         System.out.println("test");
     }
