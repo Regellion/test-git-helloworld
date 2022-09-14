aee2c32d87726e5a2e263ecc4bda36d9eb54121c
Artem Martynov <amerta90@mail.ru>
2022-09-14 09:55:42 +0300

    fix IBUS-1562

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello git!");
-        System.out.println("test");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("Hello java git");
     }
 
 }
